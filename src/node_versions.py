5b38c3081b4cd0e116787c1933a21a830bedc282
Sander Ernes <sander@baltrad.eu>
2011-11-30 07:48:21 +0000

    update versions for v0.7.8

--- src/node_versions.py
+++ src/node_versions.py
@@ -27,8 +27,8 @@ versions = {
   "HLHDF": "v0.7.9-5-gb1fc284",
   "BALTRAD-DB": "hudson-baltrad-db-284-SUCCESS",
   "BEAST": "hudson-Beast-217-SUCCESS",
-  "BALTRAD-DEX": "hudson-BaltradDex-245-SUCCESS",
-  "RAVE": "hudson-Rave-210-SUCCESS",
+  "BALTRAD-DEX": "hudson-BaltradDex-253-SUCCESS",
+  "RAVE": "hudson-Rave-217-SUCCESS",
   "RAVE-GMAP": "hudson-GoogleMapsPlugin-24-SUCCESS",
-  "BROPO": "hudson-bRopo-34-SUCCESS",
+  "BROPO": "hudson-bRopo-35-SUCCESS",
 }
