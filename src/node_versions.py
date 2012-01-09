1e47c2e51d90e6752c1deb99007fbe512f7890b9
Anders Henja <anders@baltrad.eu>
2012-01-09 10:28:04 +0100

    Upped bdb to 310

--- src/node_versions.py
+++ src/node_versions.py
@@ -25,7 +25,7 @@ Actual versions for the different software modules that should be installed
 '''
 versions = {
   "HLHDF": "v0.8.0",
-  "BALTRAD-DB": "hudson-baltrad-db-308-SUCCESS",
+  "BALTRAD-DB": "hudson-baltrad-db-310-SUCCESS",
   "BEAST": "hudson-Beast-228-SUCCESS",
   "BALTRAD-DEX": "hudson-BaltradDex-270-SUCCESS",
   "RAVE": "hudson-Rave-232-SUCCESS",
