ddc434102d14c64cc66e3bb91671a615841fe031
Anders Henja <anders@baltrad.eu>
2012-03-14 10:02:49 +0100

    Ticket 874: make generated private keys only readable by owning user

--- src/keystoreinstaller.py
+++ src/keystoreinstaller.py
@@ -21,7 +21,7 @@ Node configuration
 
 '''
 
-import os, sys
+import os, sys, stat
 import shutil
 import time
 import subprocess
@@ -29,6 +29,33 @@ import subprocess
 from installer import installer
 from InstallerException import InstallerException
 
+##
+# Function that performs chmod. Called by a walker
+# @param mode: the mode
+# @param dirname: name of directory
+# @param fnames: the filenames in the directory
+#
+def _walk_chmod(mode, dirname, fnames):
+  os.chmod(dirname, mode | stat.S_IXUSR)
+  for file in fnames:
+    if not os.path.islink(os.path.join(dirname, file)):
+      os.chmod(os.path.join(dirname, file), mode)
+
+##
+# Function that performs chown. Called by a walker
+# @param mode: the uid and gid in numerical form
+# @param dirname: name of directory
+# @param fnames: the filenames in the directory
+#
+def _walk_chown(uidgid, dirname, fnames):
+  os.chown(dirname, uidgid[0], uidgid[1])
+  for file in fnames:
+    if not os.path.islink(os.path.join(dirname, file)):
+      os.chown(os.path.join(dirname, file), uidgid[0], uidgid[1])
+
+##
+# Keystore installer
+#
 class keystoreinstaller(installer):
   ##
   # Constructor
@@ -44,7 +71,40 @@ class keystoreinstaller(installer):
       os.mkdir(dir)
     elif not os.path.isdir(dir):
       raise InstallerException, "%s exists but is not a directory"%dir
-    
+  
+  ##
+  # Sets permissions on the private key so that it only is
+  # user that is able to read it.
+  # @param env: the build environment
+  # @param dir: the directory where the key resides
+  #
+  def _setup_permissions(self, env, dir):
+    import getpass, pwd, sys
+    runas = env.getArg("RUNASUSER")
+    if runas == "root":
+      raise InstallerException, "You should not atempt to run system as root"
+
+    obj = pwd.getpwnam(runas)
+
+    if runas == getpass.getuser() or "root" == getpass.getuser():
+      if "root" == getpass.getuser():
+        os.path.walk(dir, _walk_chown, [obj.pw_uid,obj.pw_gid])
+      os.path.walk(dir, _walk_chmod, stat.S_IRUSR|stat.S_IWUSR)
+    else:
+      print "Can not set proper permissions on private key"
+      print env.expandArgs(""" Please modify permissions accordingly before continuing...
+chown -R $RUNASUSER:$RUNASUSER %s
+chmod -R 600 %s
+"""%(dir,dir))
+      print "Press return when finished or write 'quit' to exit:",
+      sys.stdout.flush()
+      a=sys.stdin.readline().strip()
+      if a == "quit":
+        raise InstallerException, "Manually terminated script"
+
+  ##
+  # Runs the keytool
+  #   
   def _keytool(self, python, *module_args):
     args = [python, "-m", "keyczar.keyczart"]
     args.extend(module_args)
@@ -78,6 +138,7 @@ class keystoreinstaller(installer):
         "--location=%s" % nodekey_priv,
         "--status=primary"
       )
+      self._setup_permissions(env, nodekey_priv)
 
     nodekey_pub = env.expandArgs("$KEYSTORE/$NODENAME.pub")
     if not os.path.exists(nodekey_pub):
