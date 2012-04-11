66c22eb8115ff49f1ff6a8aa13b65f02093e0d62
Anders Henja <anders@baltrad.eu>
2012-04-11 09:52:56 +0200

    Bumping versions for 1.1.1 release

--- src/node_versions.py
+++ src/node_versions.py
@@ -25,7 +25,7 @@ Actual versions for the different software modules that should be installed
 '''
 versions = {
   "HLHDF": "v0.8.1",
-  "BALTRAD-DB": "jenkins-baltrad-db-367",
+  "BALTRAD-DB": "jenkins-baltrad-db-373",
   "BEAST": "jenkins-Beast-257",
   "BALTRAD-DEX": "jenkins-BaltradDex-313",
   "RAVE": "jenkins-Rave-272",
