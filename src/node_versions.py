26efdd98afce786ec9c89f50541e70e84c797269
Anders Henja <anders@baltrad.eu>
2012-04-13 12:02:46 +0200

    Ticket 83: Change BltGenerateMessage from volume rule to send uuids instead of file

--- src/node_versions.py
+++ src/node_versions.py
@@ -26,9 +26,9 @@ Actual versions for the different software modules that should be installed
 versions = {
   "HLHDF": "v0.8.1",
   "BALTRAD-DB": "jenkins-baltrad-db-373",
-  "BEAST": "jenkins-Beast-259",
+  "BEAST": "jenkins-Beast-261",
   "BALTRAD-DEX": "jenkins-BaltradDex-313",
-  "RAVE": "jenkins-Rave-273",
+  "RAVE": "jenkins-Rave-274",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-26",
   "BROPO": "hudson-bRopo-37-SUCCESS",
   "BBUFR": "hudson-bbufr-5-SUCCESS",
