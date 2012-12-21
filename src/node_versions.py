f6b71763023a5c303c1c0489bef84d92e2266431
dbm <dbm@baltrad.eu>
2012-12-21 13:33:57 +0100

    Bumping versions to 1.5-beta

--- src/node_versions.py
+++ src/node_versions.py
@@ -32,5 +32,5 @@ versions = {
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-29",
   "BROPO": "jenkins-bRopo-38",
   "BBUFR": "hudson-bbufr-5-SUCCESS",
-  "BEAMB": "jenkins-beamb-52-SUCCESS"
+  "BEAMB": "jenkins-beamb-52"
 }
