839b5d378147b5dc771a777ed7ff519224095f0a
Mats Vernersson <mats.vernersson@smhi.se>
2016-04-25 12:59:58 +0200

    Bumped

--- src/node_versions.py
+++ src/node_versions.py
@@ -26,9 +26,9 @@ Actual versions for the different software modules that should be installed
 versions = {
   "HLHDF": "v0.8.3",
   "BALTRAD-DB": "jenkins-baltrad-db-394",
-  "BEAST": "jenkins-Beast-335",
+  "BEAST": "jenkins-Beast-337",
   "BALTRAD-DEX": "jenkins-BaltradDex-548",
-  "RAVE": "jenkins-Rave-522",
+  "RAVE": "jenkins-Rave-528",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-37",
   "BROPO": "jenkins-bRopo-42",
   "BBUFR": "jenkins-bbufr-8",
