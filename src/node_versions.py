673b7785bf8eb3be3c3f6bfc1af4e25bce7b0c33
Mats Vernersson <mats.vernersson@smhi.se>
2016-06-07 13:02:53 +0200

    Bumped

--- src/node_versions.py
+++ src/node_versions.py
@@ -27,7 +27,7 @@ versions = {
   "HLHDF": "v0.8.4",
   "BALTRAD-DB": "jenkins-baltrad-db-398",
   "BEAST": "jenkins-Beast-344",
-  "BALTRAD-DEX": "jenkins-BaltradDex-553",
+  "BALTRAD-DEX": "jenkins-BaltradDex-554",
   "RAVE": "jenkins-Rave-541",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-37",
   "BROPO": "jenkins-bRopo-43",
