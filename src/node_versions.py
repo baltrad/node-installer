f5e8a83b40d42b4ebed12454c1d1959b070fa114
Anders Henja <anders@henjab.se>
2016-08-25 22:45:44 +0200

    Bumped

--- src/node_versions.py
+++ src/node_versions.py
@@ -28,7 +28,7 @@ versions = {
   "BALTRAD-DB": "jenkins-baltrad-db-400",
   "BEAST": "jenkins-Beast-349",
   "BALTRAD-DEX": "jenkins-BaltradDex-558",
-  "RAVE": "jenkins-Rave-550",
+  "RAVE": "jenkins-Rave-552",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-37",
   "BROPO": "jenkins-bRopo-43",
   "BBUFR": "jenkins-bbufr-8",
