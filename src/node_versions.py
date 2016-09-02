44a26fe8f8398c85a0f3bb0df6a5bf876b9aa7e5
Mats Vernersson <mats.vernersson@smhi.se>
2016-09-02 14:35:19 +0200

    Bumped
    Bumped to include fix for Ticket 629.

--- src/node_versions.py
+++ src/node_versions.py
@@ -26,8 +26,8 @@ Actual versions for the different software modules that should be installed
 versions = {
   "HLHDF": "v0.8.4",
   "BALTRAD-DB": "jenkins-baltrad-db-400",
-  "BEAST": "jenkins-Beast-349",
-  "BALTRAD-DEX": "jenkins-BaltradDex-558",
+  "BEAST": "jenkins-Beast-350",
+  "BALTRAD-DEX": "jenkins-BaltradDex-559",
   "RAVE": "jenkins-Rave-552",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-37",
   "BROPO": "jenkins-bRopo-43",
