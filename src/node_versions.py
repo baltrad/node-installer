7b6686f4af48c65b9dcddb4640524f48f2999abe
Mats Vernersson <mats.vernersson@smhi.se>
2016-11-04 13:16:34 +0100

    Bumped
    Bumped to include fix for Ticket 650.

--- src/node_versions.py
+++ src/node_versions.py
@@ -28,7 +28,7 @@ versions = {
   "BALTRAD-DB": "jenkins-baltrad-db-400",
   "BEAST": "jenkins-Beast-358",
   "BALTRAD-DEX": "jenkins-BaltradDex-568",
-  "RAVE": "jenkins-Rave-565",
+  "RAVE": "jenkins-Rave-566",
   "RAVE-GMAP": "jenkins-GoogleMapsPlugin-37",
   "BROPO": "jenkins-bRopo-43",
   "BBUFR": "jenkins-bbufr-8",
