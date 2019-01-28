a5130f30dc4e76a798519555e43c0ba3ab03a483
Anders Henja <anders@henjab.se>
2019-01-28 08:28:59 +0100

    Bumped

--- src/node_versions.py
+++ src/node_versions.py
@@ -25,7 +25,7 @@ Actual versions for the different software modules that should be installed
 '''
 versions = {
   "HLHDF": "v0.8.9",
-  "BALTRAD-DB": "jenkins-baltrad-db-426",
+  "BALTRAD-DB": "jenkins-baltrad-db-427",
   "BEAST": "jenkins-Beast-382",
   "BALTRAD-DEX": "jenkins-BaltradDex-593",
   "RAVE": "jenkins-Rave-645",
